299781f0200052291d8c5ebc70f3f31dae44904b
breaksomeoff <alex3009898@gmail.com>
2024-06-03 17:54:21 +0200

    id-5 resolved

---
+++ src/main/java/control/CSPFilter.java
@@ -0,0 +1,21 @@
+package main.java.control;l
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CSPFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Content-Security-Policy", "frame-ancestors 'none'");
+        chain.doFilter(request, response);
+    }
+}
+
